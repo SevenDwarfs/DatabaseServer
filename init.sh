698300a6996cd9fe787572b3e50620eb4707dd6e
Kinpzz <kinpzzz@gmail.com>
2017-06-06 17:45:26 +0800

    config init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ CREATE USER 'movie_database'@'localhost' IDENTIFIED BY 'movie_database'
 GRANT ALL ON movie.* TO 'movie_database'@'localhost'
 EOF
 service mysql restart
-tail /var/log/mysql/error.log
+tail -f /var/log/mysql/error.log
