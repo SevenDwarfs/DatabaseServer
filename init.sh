0eb210d01a72217a69af93ae2e7ca5f6d0259293
Kinpzz <kinpzzz@gmail.com>
2017-06-06 23:02:55 +0800

    fix sql cmd

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 
 mysql -u root << EOF
-CREATE DATABASE movie
+CREATE DATABASE movie;
 use movie
 source init_data.sql
-CREATE USER 'movie_database'@'localhost' IDENTIFIED BY 'movie_database'
-GRANT ALL ON movie.* TO 'movie_database'@'localhost'
+CREATE USER 'movie_database'@'localhost' IDENTIFIED BY 'movie_database';
+GRANT ALL ON movie.* TO 'movie_database'@'localhost';
 EOF
 service mysql restart
 tail -f /var/log/mysql/error.log
