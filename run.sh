2bc349f307231585ef4762cda5c42c4cb92cb3ce
Kinpzz <kinpzzz@gmail.com>
2017-06-08 21:12:00 +0800

    config run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-docker stop web-server
+docker stop restful-server
 docker stop db
 docker rm db
 docker rmi db-server
 docker build -t db-server .
 docker run -d --name db db-server
-docker restart web-server
+docker restart restful-server
 
