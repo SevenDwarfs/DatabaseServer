7e92d532d86c7b127ad1f9f92efe56797c53e4fb
Kinpzz <kinpzzz@gmail.com>
2017-06-08 20:42:49 +0800

    fix run.sh docker build error

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ docker stop web-server
 docker stop db
 docker rm db
 docker rmi db-server
-docker build -t db-server 
+docker build -t db-server .
 docker run -d --name db db-server
 docker restart web-server
 
